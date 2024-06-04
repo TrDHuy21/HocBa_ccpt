20c52bd7e65cdae6a363a3e9b7ebf469b7a4c96f
Tran Duc Huy <vlsvhuy@gmail.com>
2024-06-04 10:07:09 +0700

    start project

---
+++ hocbadientu/src/main/java/ccpt/nhom2/hocbadientu/HocbadientuApplication.java
@@ -0,0 +1,13 @@
+package ccpt.nhom2.hocbadientu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HocbadientuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HocbadientuApplication.class, args);
+	}
+
+}
---
+++ hocbadientu/src/test/java/ccpt/nhom2/hocbadientu/HocbadientuApplicationTests.java
@@ -0,0 +1,13 @@
+package ccpt.nhom2.hocbadientu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HocbadientuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
